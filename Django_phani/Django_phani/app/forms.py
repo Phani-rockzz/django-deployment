4ac7d2f01e94c336bbae8aba5f72738225fd846f
Phanindra mekapotula <mekapotulaphani@gmail.com>
2020-04-19 21:13:48 +0530

    second

--- Django_phani/Django_phani/app/forms.py
+++
@@ -1,8 +0,0 @@
-from app import models
-from app.models import User
-from django import forms
-
-class NewUser(forms.ModelForm):
-    class Meta:
-        Model = User
-        fields = '__all__'
--- Django_phani/Django_phani/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\visual studio\Django_phani\Django_phani\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
